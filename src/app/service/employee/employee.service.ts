df6775ec8b2ffe45686a1d2939ab879f3c26e20d
Senuka Bandara <senukabandara2006@gmail.com>
2025-04-29 11:35:40 +0530

    Create  employee service functionality

--- src/app/service/employee/employee.service.ts
+++ src/app/service/employee/employee.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { DepartmentType } from '../../model/util/DepartmentType';
+import { Observable } from 'rxjs';
+import { Employee } from '../../model/Employee';
 
 @Injectable({
   providedIn: 'root'
@@ -10,12 +13,32 @@ export class EmployeeService {
 
   constructor(private http: HttpClient) { }
 
-  getEmployees() {
-    return this.http.get(`${this.baseUrl}`);
+  getEmployees() : Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.baseUrl}`);
   }
 
-  getEmployeesByDepartment() {
-    
+  getEmployeesByDepartment(department: DepartmentType) : Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.baseUrl}?department=${department}`);
+  }
+
+  getEmployeesByName(name: string) : Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.baseUrl}?name=${name}`);
+  }
+
+  getEmployeeByEmail(email: string) : Observable<Employee> {
+    return this.http.get<Employee>(`${this.baseUrl}/${email}`);
+  }
+
+  createEmployee(employee : Employee) : Observable<Employee> {
+    return this.http.post<Employee>(`${this.baseUrl}`, employee);
+  }
+
+  updateEmployee(employee: Employee) : Observable<Employee> {
+    return this.http.put<Employee>(`${this.baseUrl}`, employee);
+  }
+
+  deleteEmployeeByID(id: number) : Observable<Boolean> {
+    return this.http.delete<Boolean>(`${this.baseUrl}/${id}`);
   }
 
 }
