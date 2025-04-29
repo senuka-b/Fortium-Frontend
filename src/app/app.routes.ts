e17a862498c7546d75d1052ea934ef5eab77a1ea
Senuka Bandara <senukabandara2006@gmail.com>
2025-04-29 14:47:12 +0530

    Create manage employee

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './shared/components/dashboard/dashboard.component';
 import { AddEmployeeComponent } from './shared/components/add-employee/add-employee.component';
 import { ViewEmployeesComponent } from './shared/components/view-employees/view-employees.component';
+import { ManageEmployeeComponent } from './shared/components/manage-employee/manage-employee.component';
 
 export const routes: Routes = [
     {
@@ -16,6 +17,10 @@ export const routes: Routes = [
             {
                 path: "view-employees",
                 component: ViewEmployeesComponent
+            },
+            {
+                path: "manage-employees",
+                component: ManageEmployeeComponent
             }
         ]
         
