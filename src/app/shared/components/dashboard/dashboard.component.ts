c5465fddecf7930a43132401074f0dba5fba07f0
Senuka Bandara <senukabandara2006@gmail.com>
2025-04-29 11:52:22 +0530

    Create router-outlet in dashboard

--- src/app/shared/components/dashboard/dashboard.component.ts
+++ src/app/shared/components/dashboard/dashboard.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
