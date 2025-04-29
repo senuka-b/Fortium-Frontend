d7e4958067e80a1d62544fe38b9cc376eaa36568
Senuka Bandara <senukabandara2006@gmail.com>
2025-04-29 16:15:49 +0530

    Create login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,16 @@ import { AddEmployeeComponent } from './shared/components/add-employee/add-emplo
 import { ViewEmployeesComponent } from './shared/components/view-employees/view-employees.component';
 import { ManageEmployeeComponent } from './shared/components/manage-employee/manage-employee.component';
 import { ExportComponent } from './shared/components/export/export.component';
+import { LoginComponent } from './shared/components/login/login.component';
 
 export const routes: Routes = [
     {
+        path: "",
+        component: LoginComponent
+    },
+    {
         path: "dashboard",
-        component: DashboardComponent,
+        component: ViewEmployeesComponent,
 
         children: [
             {
---
+++ src/app/shared/components/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  username: string = '';
+  password: string = '';
+
+  login() {}
+
+  signup() {}
+
+}
